d8d2ab163ecfc19ae2909c0ea5f86e33b145b7f9
Pablo Ferres <pabloferres96@gmail.com>
2020-10-26 20:03:10 +0100

    Update book.js

--- Tema3/Worksheet5/book.js
+++ Tema3/Worksheet5/book.js
@@ -21,38 +21,54 @@ class BookList {
         if (Book.read == true){
             this.booksRead ++;
             this.books.push(Book);
-            this.lastBook = book.title;
+            this.lastBook = Book.title;
         }else{
             this.booksNotRead ++;
             this.books.push(Book);
+            if (this.actualBook == null){
+                this.actualBook = Book.title;
+            }
+            if (this.nextBook == null && Book.title != this.actualBook){
+                this.nextBook = Book.title;
+            }
         }
-        if (this.actualBook == null){
-            this.actualBook = Book.title;
-        }
+
     }
     listarLibros(){
         console.log(this.books);
         console.log(this.actualBook);
+        console.log(this.nextBook);
     }
     finishCurrentBook(){
-        Book.read = true;
-        Book.readDate = Date.now();
-        this.lastBook = Book.title;
+        let pos = 0;
+        for (let i=0; i<this.books.length; i++){
+            if (this.books[i].title == this.actualBook){
+                pos = i;
+                this.books[i].readDate = Date.now();
+                this.books[i].read = true;
+                
+            }
+        }
+        console.log(pos);
+        this.lastBook = this.actualBook;
         let control = false;
         this.actualBook = this.nextBook;
+        this.nextBook = "";
         for (let i=0; i<this.books.length; i++){
-            if (this.books[i].read == false && control == false ){
-                this.nextBook = books(i).title;
+            if (this.books[i].read == false && control == false && this.books[i].title != this.actualBook){
+                this.nextBook = this.books[i].title;
                 control = true;
             }
-        }   
+        }  
     }
 }
 let libro = new Book("theWitcher", "fantasia", "Pepe", false);
 let libro2 = new Book("theWitcher2", "aventura", "Pepe", false);
+let libro3 = new Book("theWitcher3", "fantasia", "Pepe", false);
 let array = new Array();
 let lista = new BookList(0, 0, null, null, null, array);
 lista.addBook(libro);
 lista.addBook(libro2);
-//lista.finishCurrentBook();
+lista.addBook(libro3);  
+lista.finishCurrentBook();
 lista.listarLibros();
